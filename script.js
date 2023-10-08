62d878761c27e8b7daea7ef0d1e71e009d906500
patrickashi <patrickpeko1@gmail.com>
2023-10-08 14:08:52 +0100

    added new files

---
+++ script.js
@@ -0,0 +1,101 @@
+
+
+
+function toggleAnswer11() {
+    const answer = document.getElementById("answer11");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer10() {
+    const answer = document.getElementById("answer10");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer9() {
+    const answer = document.getElementById("answer9");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer8() {
+    const answer = document.getElementById("answer8");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer7() {
+    const answer = document.getElementById("answer7");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer6() {
+    const answer = document.getElementById("answer6");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer5() {
+    const answer = document.getElementById("answer5");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer4() {
+    const answer = document.getElementById("answer4");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer1() {
+    const answer = document.getElementById("answer1");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer2() {
+    const answer = document.getElementById("answer2");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
+
+function toggleAnswer3() {
+    const answer = document.getElementById("answer3");
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
